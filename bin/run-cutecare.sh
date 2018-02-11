48ad7faf2d15d9a70c0e161bb1664a9f670f3915
Evgeny Savitsky <evgeny.savitsky@devprom.ru>
2018-02-11 21:13:34 +0300

    single script has been added

---
+++ bin/run-cutecare.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -xe
+if [ ! -f DeepSpeech.py ]; then
+    echo "Please make sure you run this from DeepSpeech's top level directory."
+    exit 1
+fi;
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/DeepSpeech/native_client
+
+python -u DeepSpeech.py \
+  --train_files data/cutecare/cutecare-train.csv \
+  --dev_files data/cutecare/cutecare-dev.csv \
+  --test_files data/cutecare/cutecare-test.csv \
+  --checkpoint_dir /work/checkpoint \
+  --export_dir /work/export \
+  --lm_binary_path /work/lm.binary \
+  --lm_trie_path /work/trie
+  --train_batch_size 13 \
+  --dev_batch_size 13 \
+  --test_batch_size 13 \
+  --epoch 60 \
+  --learning_rate 0.0001 \
+  --display_step 10 \
+  --validation_step 1 \
+  --dropout_rate 0.15 \
+  --default_stddev 0.046875 \
+  --checkpoint_step 1 \
+  --wer_log_pattern "GLOBAL LOG: logwer('${COMPUTE_ID}', '%s', '%s', %f)"\
+  "$@"
