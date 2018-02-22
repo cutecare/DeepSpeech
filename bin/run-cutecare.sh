846da752e9c711898854a61b97e9065d754e45d4
CuteCare <33788776+cutecare@users.noreply.github.com>
2018-02-22 10:04:07 +0300

    Update run-cutecare.sh

--- bin/run-cutecare.sh
+++ bin/run-cutecare.sh
@@ -22,7 +22,7 @@ python -u DeepSpeech.py \
   --validation_step 1 \
   --dropout_rate 0.15 \
   --default_stddev 0.046875 \
-  --n_hidden 8192 \
+  --n_hidden 4096 \
   --checkpoint_step 1 \
   --early_stop false \
   --wer_log_pattern "GLOBAL LOG: logwer('${COMPUTE_ID}', '%s', '%s', %f)"\
